1ed962cff7ee53c88ae8948ec586bd6653dcb463
chathurperera <chathuraperera007@gmail.com>
2022-11-05 21:38:17 +0530

    token verification middleware added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,7 +32,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
 //@desc Get user data
 //@route GET /api/users/me
-//@access public
+//@access Private
 const getMe = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "use data" });
 });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,12 @@ const {
   getMe,
   loginUser,
 } = require("../controllers/userController");
+const protect = require("../middleware/authMiddleware");
 const router = express.Router();
+
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.get("/me", getMe);
+router.get("/me", protect , getMe);
 
 module.exports = router;
